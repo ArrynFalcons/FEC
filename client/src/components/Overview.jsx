c68b866d412e02e15be7a9d5cf85581e2b9618fd
Shloka <shlokareddy@gmail.com>
2022-03-31 10:16:46 -0700

    created react skeleton

---
+++ client/src/components/Overview.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Overview = (props) => (
+
+  <div>Overview</div>
+
+);
+
+export default Overview;
---
+++ client/src/components/Questions.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Questions = (props) => (
+
+  <div>Questions</div>
+
+);
+
+export default Questions;
