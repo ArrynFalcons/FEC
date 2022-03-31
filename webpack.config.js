5efe4857e2d01a7955ad9b9570c7f549c957d1c9
Joel Hench <joelhench@Olivias-MacBook-Pro-3.local>
2022-03-30 17:21:41 -0700

    Installed packages

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+var SRC_DIR = path.join(__dirname, "client/src");
+var DIST_DIR = path.join(__dirname, "client/dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  }
+}
+
