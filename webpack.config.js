914b4e256ce1c16aa5c4bc07f16343a6246eddc7
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-03-31 12:02:26 -0700

    Fixes webpack lint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
-var SRC_DIR = path.join(__dirname, "client/src");
-var DIST_DIR = path.join(__dirname, "client/dist");
+const SRC_DIR = path.join(__dirname, 'client/src');
+const DIST_DIR = path.join(__dirname, 'client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: DIST_DIR,
   },
   module: {
@@ -17,19 +17,18 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(js|jsx)?/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
-    ]
-  }
-}
-
+    ],
+  },
+};
